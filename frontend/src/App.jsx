66db10fe8af99e6f0941dd56a019101dec47262a
cmmurgo <cmmurgo@gmail.com>
2025-05-08 18:20:25 -0300

    boostrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import RegisterUser from './pages/RegisterUser';
+import './App.css'; 
 
 function App() {
   return (
