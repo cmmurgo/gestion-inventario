a7d9dd3ad88fc7d77f22bb5e00ecd18df9b2f40e
cmmurgo <cmmurgo@gmail.com>
2025-05-11 10:32:37 -0300

    vuelta

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
 require('dotenv').config();
-
 const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/usuarios');
 
-// Middlewares primero
 app.use(cors());
 app.use(express.json());
-
-// Luego las rutas
 app.use('/api/auth', authRoutes);
-app.use('/api/usuarios', userRoutes);
 
-// Puerto
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000; //Para Producción
+//const PORT = process.env.PORT || 3001; //Para Local
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
