5d26b92d3d4024f69fffb0a9c420d0008cea8d9a
cmmurgo <cmmurgo@gmail.com>
2025-05-11 10:38:00 -0300

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -15,20 +15,3 @@ const pool = new Pool({
 
 module.exports = pool;
 
-//Local:
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT || 5432,
-//   // ssl: {
-//   //   rejectUnauthorized: false, // Puede que sea necesario en algunos entornos
-//   //   sslmode: process.env.SSL_MODE, // Asegura el uso de SSL
-//   // },
-// });
-
-// module.exports = pool;
