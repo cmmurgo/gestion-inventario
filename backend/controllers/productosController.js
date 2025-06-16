db8ba9d7112b409bf8a387413a299354df79bc38
paez.zanini <paez.zanini@gmail.com>
2025-06-16 15:53:32 -0300

    fix(productos) mensajes backend

--- backend/controllers/productosController.js
+++ backend/controllers/productosController.js
@@ -46,7 +46,7 @@ exports.update = async (req, res) => {
 exports.delete = async (req, res) => {
   try {
     await productoModel.delete(req.params.id);
-    res.json({ message: 'Producto eliminado (baja lógica)' });
+    res.json({ message: 'Producto dado de baja correctamente' });
   } catch (error) {
     console.error('Error al eliminar producto:', error);
     res.status(500).json({ message: 'Error al eliminar producto', error });
